4321764108e787965568ed6e27da24e360c3c7b5
KingUranus <KingUranus@qq.com>
2019-06-21 01:54:05 +0800

    add 7.Flask开发猜数字小游戏---聪明的奥特曼

---
+++ 7.FlaskGuessCards/app.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Author   : 王翔
+# @JianShu  : 清风Python
+# @Date     : 2019/6/20 22:24
+# @Software : PyCharm
+# @version  ：Python 3.7.3
+# @File     : app.py
+
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    number_list = random.sample(list(range(1, 10)), 9)
+    return render_template('index.html', num_list=number_list)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
